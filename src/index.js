e0239c0a404f2727eb8e3cbccb3640ae8e0241e9
Le QuangDung <quangdung100194@gmail.com>
2019-11-14 16:13:09 +0700

    Keep calm and commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 function mutiply(a,c) {
   return a + c;
 }
 mutiply();
 app.listen(3100, ()=>{
-  console.log("server is running 1");
+  console.log('server is running 121');
 });
