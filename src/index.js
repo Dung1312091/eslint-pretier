15be83ae4639c7324aed8800726fbb8d2ec7cb8c
Le QuangDung <quangdung100194@gmail.com>
2019-11-15 10:42:08 +0700

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-function mutiply(a,c) {
+
+function mutiply(a, c) {
   console.log('aaaaa');
   return a + c;
 }
 mutiply();
-app.listen(3100, ()=>{
+app.listen(3100, () => {
   console.log('server is running 121');
 });
